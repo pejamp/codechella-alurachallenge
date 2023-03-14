82e73c9ba57682e5bbb91205d2f92f20a84dee94
Pedro Rodrigues <pedro.roguea@gmail.com>
2023-03-13 21:31:42 -0300

    feat: improve responsive style on home page

--- src/layout-patterns/lineup-day/index.tsx
+++ src/layout-patterns/lineup-day/index.tsx
@@ -7,33 +7,25 @@ export function LineupDay() {
         <h3>SÁBADO {'<'}11/03{'>'}</h3>
       </div>
       <div className='lineup__attractions'>
-        <div className='attractions'>
-          <h4 className='attractions__headliner'>System of a DOM</h4>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__second'>Python Maiden</span>
-          <span className='attractions__second'>Apollo Client 2001</span>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__second'>Bon Java</span>
-          <span className='attractions__second'>NickCallback</span>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__third'>Linkin Promises</span>
-          <span className='attractions__third'>Fullstack Fighters</span>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__third'>Papa React</span>
-          <span className='attractions__third'>Angular in Chains</span>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__fourth'>Agnostic Front-end</span>
-          <span className='attractions__fourth'>SlipkNode</span>
-        </div>
-        <div className='attractions'>
-          <span className='attractions__fourth'>Pink Flutter</span>
-          <span className='attractions__fourth'>Kiss</span>
-        </div>
+        <h3 className='attractions__headliner'>System of a DOM</h3>
+      
+        <h4 className='attractions__second'>Python Maiden</h4>
+        <h4 className='attractions__second'>Apollo Client 2001</h4>
+      
+        <h4 className='attractions__second'>Bon Java</h4>
+        <h4 className='attractions__second'>NickCallback</h4>
+      
+        <h5 className='attractions__third'>Linkin Promises</h5>
+        <h5 className='attractions__third'>Fullstack Fighters</h5>
+    
+        <h5 className='attractions__third'>Papa React</h5>
+        <h5 className='attractions__third'>Angular in Chains</h5>
+    
+        <h6 className='attractions__fourth'>Agnostic Front-end</h6>
+        <h6 className='attractions__fourth'>SlipkNode</h6>
+      
+        <h6 className='attractions__fourth'>Pink Flutter</h6>
+        <h6 className='attractions__fourth'>Kiss</h6> 
       </div>
     </div>
   );
