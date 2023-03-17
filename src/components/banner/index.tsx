bddb427e34f5b425a06d5210d6bd81c730b58f21
Pedro Rodrigues <pedro.roguea@gmail.com>
2023-03-17 05:01:31 -0300

    chore: add responsive style on information page

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -8,7 +8,8 @@ interface BannerProps {
 
 export function Banner({ bannerText, bannerImage, hashtagCodeChella }: BannerProps) {
   return (
-    <section className='banner' style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${bannerImage})` }}>
+    <section className='banner'>
+      <div className="banner__image" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${bannerImage})` }}></div>
       <h1 className='banner__text'>{bannerText}</h1>
       { hashtagCodeChella && <span>#CodeChella2023!</span> }
     </section>
--- src/pages/information/index.tsx
+++ src/pages/information/index.tsx
@@ -16,10 +16,10 @@ export function Information() {
           <h2>Perguntas Frequentes:</h2>
           <div className="questions__accordion">
             <Accordion title={"Quais serão as atrações?"} content={'Teremos dois dias de shows, o primeiro (11/03) com bandas de rock, e o segundo (12/03) com bandas pop. Confira o line-up em detalhes clicando neste link!'} />
-            <Accordion title={"Qual é a classificação etária?"} content={''} />
-            <Accordion title={"Quais são os setores disponíveis?"} content={''} />
-            <Accordion title={"Quais são os itens proibidos?"} content={''} />
-            <Accordion title={"Quais são os itens proibidos?"} content={''} />
+            <Accordion title={"Qual é a classificação etária?"} content={'A partir de 16 anos, todo mundo pode entrar. De 13 a 15 anos, tem que estar acompanhado dos pais ou responsáveis legais. Menores de 10 anos não podem entrar.'} />
+            <Accordion title={"Quais são os setores disponíveis?"} content={"Pista Premium Pista Comum Cadeiras térreo Cadeiras superiores Rampas"} />
+            <Accordion title={"Quais são os itens proibidos?"} content={'Garrafas, latas, bebidas, utensílios de armazenagem, embalagens rígidas com tampa, capacetes, cadeiras ou bancos, armas de fogo e armas brancas, objetos pontiagudos, cortantes e/ou perfurantes, correntes e cinturões, fogos de artifício, objetos de vidro, substâncias inflamáveis, cigarro eletrônico, corrosivas e ou tóxicas, revistas, jornais, livros e copo térmico ou similares.'} />
+            <Accordion title={"Quem tem direito a meia-entrada?"} content={'De acordo com a Lei Federal, têm direito ao benefício da meia-entrada aqueles que preencherem os requisitos e apresentarem os documentos comprobatórios.'} />
           </div>
         </section>
       </main>
